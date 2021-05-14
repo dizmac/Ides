b2fedb032b9b631881e82c0a810399c6149da1bb
PeacefullNoob <8128725-PeacefullNoob@users.noreply.gitlab.com>
2021-05-14 22:10:06 +0000

    Add new file

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ping',
+    args: false,
+    execute(message) {
+		message.channel.send('Loading data').then (async (msg) =>{
+			msg.delete()
+			message.channel.send(`Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        })
+    }
+});
