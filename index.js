8a9ca5e9f41e44de2617e01e7c4c16a78d95e537
PeacefullNoob <8128725-PeacefullNoob@users.noreply.gitlab.com>
2021-05-15 23:27:48 +0000

    Ignores if user is bot dev if it works

--- index.js
+++ index.js
@@ -53,16 +53,20 @@ client.on('message', async message => {
     const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 2) * 1000;
 
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+    if (!authorized.includes(message.author.id)) { //Ignores if user is bot dev
+        if (timestamps.has(message.author.id)) { 
+            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-        if (now < expirationTime) {
-	        const timeLeft = (expirationTime - now) / 1000;
-	        return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing this command.`);
+            if (now < expirationTime) {
+	            const timeLeft = (expirationTime - now) / 1000;
+	            return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing this command.`);
+                }
 	        }
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
         }
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+
 
 
     try {
