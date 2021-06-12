895ce1cc9704e84bf711da614e7ca983fe666ce0
PeacefullNoob <8128725-PeacefullNoob@users.noreply.gitlab.com>
2021-06-12 12:32:37 +0000

    Fixed people being able to ping @here and @everyone if the bots has the right perms

--- commands/schedule.js
+++ commands/schedule.js
@@ -17,6 +17,7 @@ module.exports = {
         let num, host, counter = 0, data = Cache.getCache(division), eventCount = data.length;
 
         if (args[1] && args[1].startsWith('host=')) {
+            if (args[1] === 'host=@here' || args[1] === 'host=@everyone') return message.channel.send('Invalid argument!');
             host = args[1].split('=')[1];
             data = filter.filterHost(data, host);
         } else {
